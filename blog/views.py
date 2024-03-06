89038076e3fe37fc0f16a8823931036236f512d9
uly <ulyana.akinfina22@gmail.com>
2024-03-06 18:21:40 +0300

    first view post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+def post_list(request): # обязательный объект, содержащтй информацию о запросе
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+    # render() - функция сокращенного доступа, «охватывает» несколько уровней MVC
+
+
+def post_detail(request, id): # представление детальной информации о посте
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+
+
+
+
+
+
+
