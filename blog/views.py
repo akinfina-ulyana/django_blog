48ee27c498e846012b9234f0db41dec96f8c2bab
uly <ulyana.akinfina22@gmail.com>
2024-03-08 13:31:56 +0300

    get_object_or_404 for specific article

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
 def post_list(request): # обязательный объект, содержащтй информацию о запросе
-    posts = Post.published.all()
+    posts = Post.published.all()  # достаём все опубликованные посты
     return render(request, 'blog/post/list.html', {'posts': posts})
     # render() - функция сокращенного доступа, «охватывает» несколько уровней MVC
 
 
-def post_detail(request, id): # представление детальной информации о посте
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+def post_detail(request, id): # представление детальной информации о посте (опубликованном)
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+
+
 
 
 
